889deab247baf6620b6078b3197f977e393d5203
samsos <limbu.samyog.828@gmail.com>
2026-03-31 02:03:58 +0545

    redshift/gammastep + scripts

---
+++ scripts/updatedots.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+cp -r ~/.config/foot ~/dotfiles/.config/
+cp -r ~/.config/fastfetch ~/dotfiles/.config/
+cp -r ~/.config/mako ~/dotfiles/.config/
+cp -r ~/.config/nvim ~/dotfiles/.config/
+cp -r ~/.config/rofi ~/dotfiles/.config/
+cp -r ~/.config/sway ~/dotfiles/.config/
+cp -r ~/.config/waybar ~/dotfiles/.config/
+cp -r ~/.config/gammastep/ ~/dotfiles/.config/
+cp ~/.bashrc ~/dotfiles/
+cp ~/scripts/updatedots.sh ~/dotfiles/scripts/
+cd ~/dotfiles
+git add --all
+git commit
+git push
+cd -
