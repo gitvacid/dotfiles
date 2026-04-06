4b77a43604c973f245a06f4e3b4388398bd9ca26
gitvacid <limbu.samyog.828@gmail.com>
2026-04-07 00:05:03 +0545

    vim.pack is so much nicer

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -16,21 +16,39 @@ vim.o.scrolloff = 18
 
 -- keybindings
 local map = vim.keymap.set
+map('n', '<leader>e', vim.cmd.Ex)
 map('n', '<leader>w', vim.cmd.write)
-map({'n', 'i'}, '<C-s>', vim.cmd.write)
 map('n', '<leader>q', vim.cmd.quit)
 map('n', '<leader>o', vim.cmd.source)
 map('n', '<Esc>', vim.cmd.nohlsearch)
+
+map({'n', 'i'}, '<C-s>', ':w<CR>')
+map('n', '<C-a>', 'gg<S-v>G')
+
 map({'n','v'}, '<leader>y', '"+y')
 map({'n','v'}, '<leader>p', '"+p')
+
 map('n', '<C-d>', '<C-d>zz')
 map('n', '<C-u>', '<C-u>zz')
-map('n', '<C-a>', 'gg<S-v>G')
+
 map('n', '<leader>sh', ':sp<CR>')
 map('n', '<leader>sv', ':vsp<CR>')
 map('n', '<C-h>', '<C-w><C-h>')
 map('n', '<C-j>', '<C-w><C-j>')
 map('n', '<C-k>', '<C-w><C-k>')
 map('n', '<C-l>', '<C-w><C-l>')
+
 map('n', '<leader>t', ':sp term://bash<CR>')
 map('t', '<Esc>', '<C-\\><C-n>')
+
+vim.pack.add({
+  "https://github.com/vague-theme/vague.nvim",
+  "https://github.com/brenoprata10/nvim-highlight-colors", 
+})
+
+-- colors
+require('nvim-highlight-colors').setup({})
+require("vague").setup({
+	italic = false,
+})
+vim.cmd("colorscheme vague")
