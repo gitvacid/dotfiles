aaddd0b77569fcdaf38d6d9666d4d0070e6c3385
gitvacid <limbu.samyog.828@gmail.com>
2026-04-07 19:22:16 +0545

    FIX: neovim clipboard binding: insert mode instead of visual mode

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,4 @@
--- soso neovim 12+ config 
-vim.g.mapleader = ' '
-vim.g.maplocalleader = '\\'
-
--- options
+-- OPTIONS
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.cursorline = true
@@ -10,40 +6,13 @@ vim.o.swapfile = false
 vim.o.termguicolors = true
 vim.o.splitright = true
 vim.o.splitbelow = true
-vim.o.shiftwidth = 4
-vim.o.tabstop = 4
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
 vim.o.scrolloff = 18
 vim.o.winborder = 'bold'
 vim.o.signcolumn = 'yes'
 
--- keybindings
-local map = vim.keymap.set
-map('n', '<leader>e', vim.cmd.Ex)
-map('n', '<leader>w', vim.cmd.write)
-map('n', '<leader>q', vim.cmd.quit)
-map('n', '<leader>o', vim.cmd.source)
-map('n', '<Esc>', vim.cmd.nohlsearch)
-
-map({'n', 'i'}, '<C-s>', ':w<CR>')
-map('n', '<C-a>', 'gg<S-v>G')
-
-
-map({'n', 'i'}, '<leader>y', '"+y')
-map({'n', 'i'}, '<leader>p', '"+p')
-
-map('n', '<C-d>', '<C-d>zz')
-map('n', '<C-u>', '<C-u>zz')
-
-map('n', '<leader>sh', ':sp<CR>')
-map('n', '<leader>sv', ':vsp<CR>')
-map('n', '<C-h>', '<C-w><C-h>')
-map('n', '<C-j>', '<C-w><C-j>')
-map('n', '<C-k>', '<C-w><C-k>')
-map('n', '<C-l>', '<C-w><C-l>')
-
-map('n', '<leader>t', ':sp term://bash<CR>')
-map('t', '<Esc>', '<C-\\><C-n>')
-
+-- INSTALL PLUGINS
 vim.pack.add({
   'https://github.com/vague-theme/vague.nvim',
   'https://github.com/brenoprata10/nvim-highlight-colors', 
@@ -52,25 +21,46 @@ vim.pack.add({
   'https://github.com/nvim-treesitter/nvim-treesitter',
 })
 
-require('cord').setup({})
--- colors
+-- KEYBINDINGS
+vim.g.mapleader = " "
+local map = vim.keymap.set
+map('n', '<leader>e', vim.cmd.Ex)
+map('n', '<leader>w', vim.cmd.write)
+map('n', '<leader>q', vim.cmd.quit)
+map('n', '<leader>o', vim.cmd.source)
+map('n', '<Esc>', vim.cmd.nohlsearch)
+map('n', '<C-a>', 'gg<S-v>G')
+map('n', '<C-d>', '<C-d>zz')
+map('n', '<C-u>', '<C-u>zz')
+-- clipboard
+map({'n', 'v'}, '<leader>y', '"+y')
+map({'n', 'v'}, '<leader>p', '"+p')
+-- windows
+map('n', '<leader>sh', ':sp<CR>')
+map('n', '<leader>sv', ':vsp<CR>')
+map('n', '<C-h>', '<C-w><C-h>')
+map('n', '<C-j>', '<C-w><C-j>')
+map('n', '<C-k>', '<C-w><C-k>')
+map('n', '<C-l>', '<C-w><C-l>')
+-- terminal
+map('n', '<leader>t', ':sp term://bash<CR>')
+map('t', '<Esc>', '<C-\\><C-n>')
+-- diagnostics
+map('n', '<leader>d', ':lua vim.diagnostic.open_float()<CR>')
+
+-- PLUGINS SETUP
 require('vague').setup({
 	italic = false,
 })
 require('nvim-highlight-colors').setup({})
 vim.cmd('colorscheme vague')
 
-
--- lsp & stuff
-vim.lsp.enable({
-	'clangd',
-})
--- treesitter highlighting
+-- LSP & stuff
+vim.lsp.enable({'clangd'})
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'c', },
+  pattern = { 'c', 'lua' },
   callback = function() vim.treesitter.start() end,
 })
-
 -- lsp-autocomplete,  grabbed it straight from the docs
 vim.cmd[[set completeopt+=menuone,noselect,popup]]
 vim.api.nvim_create_autocmd('LspAttach', {
@@ -80,18 +70,13 @@ vim.api.nvim_create_autocmd('LspAttach', {
 		if client:supports_method('textDocument/implementation') then
 			-- Create a keymap for vim.lsp.buf.implementation ...
 		end
-
-		-- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
+		-- Enable auto-completion
 		if client:supports_method('textDocument/completion') then
-			-- Optional: trigger autocompletion on EVERY keypress. May be slow!
 			local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
 			client.server_capabilities.completionProvider.triggerCharacters = chars
-
 			vim.lsp.completion.enable(true, client.id, ev.buf, {autotrigger = true})
 		end
-
-		-- Auto-format ('lint') on save.
-		-- Usually not needed if server supports 'textDocument/willSaveWaitUntil'.
+		-- Auto-format ('lint') on save
 		if not client:supports_method('textDocument/willSaveWaitUntil')
 			and client:supports_method('textDocument/formatting') then
 			vim.api.nvim_create_autocmd('BufWritePre', {
